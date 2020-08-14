1807e6a5abbe732985f1360db7c390e613ac9985
Neythen <zcqsntr@ucl.ac.uk>
2020-08-14 18:56:47 +0100

    Set up on chemostat system

---
+++ chemostat_system/fitted_Q_param_inf.py
@@ -0,0 +1,216 @@
+import sys
+import os
+
+IMPORT_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'imports')
+
+sys.path.append(IMPORT_PATH)
+
+import math
+from casadi import *
+import numpy as np
+import matplotlib.pyplot as plt
+from OED_env import *
+from DQN_agent import *
+import tensorflow as tf
+import time
+
+from ROCC import *
+from xdot import xdot
+
+def disablePrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+
+
+if __name__ == '__main__':
+    #sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+
+    n_episodes = 1
+    if len(sys.argv) == 3:
+        if sys.argv[2] == '1':
+
+            n_episodes = 10000
+        elif sys.argv[2] == '2':
+            n_episodes = 50000
+        else:
+            n_episodes = 100000
+
+        save_path =  sys.argv[1] + sys.argv[2]+'/'
+        print(n_episodes)
+        os.makedirs(save_path, exist_ok = True)
+    elif len(sys.argv) == 2:
+        save_path =  sys.argv[1] +'/'
+        os.makedirs(save_path, exist_ok = True)
+    else:
+        save_path = './'
+
+
+
+
+    all_returns = []
+
+    #y, y0, umax, Km, Km0, A
+    actual_params = DM([480000, 480000, 520000, 520000, 1, 1.1, 0.00048776, 0.000000102115, 0.00006845928, 0.00006845928])#, DM([[0, 0],[0, 0]])]
+    actual_params = DM([1, 1.1, 0.00048776, 0.000000102115, 0.00006845928, 0.00006845928])#, DM([[0, 0],[0, 0]])]
+
+    input_bounds = [0, 0.1]
+    n_controlled_inputs = 4
+
+    n_params = actual_params.size()[0]
+    n_system_variables = 2
+    n_FIM_elements = sum(range(n_params + 1))
+
+    n_tot = n_system_variables + n_params * n_system_variables + n_FIM_elements
+    print(n_params, n_system_variables, n_FIM_elements)
+    num_inputs = 5  # number of discrete inputs available to RL
+
+    dt = 1 / 3000
+
+
+    param_guesses = actual_params
+    y0 = [20000, 30000., 0, 0, 1]
+
+    agent = KerasFittedQAgent(layer_sizes=[72, 150, 150, 150, num_inputs**n_controlled_inputs])
+
+    N_control_intervals = 100
+    control_interval_time = 1
+
+    n_observed_variables = 2
+
+    env = OED_env(y0, xdot, param_guesses, actual_params, n_observed_variables, n_controlled_inputs, num_inputs, input_bounds, dt, control_interval_time,1)
+    explore_rate = 1
+    for episode in range(n_episodes):
+
+        env.reset()
+        state = env.get_initial_RL_state()
+
+        e_return = 0
+        e_actions =[]
+        e_rewards = []
+        trajectory = []
+        #actions = [9,4,9,4,9,4]
+
+        for e in range(0, N_control_intervals):
+            t = time.time()
+            action = agent.get_action(state, explore_rate)
+
+
+            next_state, reward, done, _ = env.step(action)
+
+
+
+            if e == N_control_intervals - 1:
+                next_state = [None]*24
+                done = True
+            transition = (state, action, reward, next_state, done)
+            trajectory.append(transition)
+
+            e_actions.append(action)
+            e_rewards.append(reward)
+
+            state = next_state
+            e_return += reward
+
+
+        agent.memory.append(trajectory)
+
+        #train the agent
+        skip = 200
+        if episode % skip == 0 or episode == n_episodes - 2:
+            #explore_rate = agent.get_rate(episode, 0, 1, n_episodes / 10)
+            if explore_rate == 1:
+                n_iters = 0
+            elif len(agent.memory[0]) * len(agent.memory) < 10000:
+                n_iters = 1
+            elif len(agent.memory[0]) * len(agent.memory) < 20000:
+                n_iters = 1
+            elif len(agent.memory[0]) * len(agent.memory) < 40000:
+                n_iters = 1
+            else:
+                n_iters = 2
+
+            t = time.time()
+            for iter in range(n_iters):
+                print(iter, n_iters)
+                agent.fitted_Q_update()
+                print()
+            print('fitting time: ', time.time() -t)
+
+        all_returns.append(e_return)
+
+        '''
+        trajectory = trajectory_solver(y0, us, actual_params)
+        all_ys.append(trajectory.elements()[-1])
+        '''
+
+        if episode %skip == 0 or episode == n_episodes -1:
+            print()
+            print('EPISODE: ', episode)
+            print('explore rate: ', explore_rate)
+            print('return: ', e_return)
+            print('av return: ', np.mean(all_returns[-skip:]))
+            print('actions:', e_actions)
+            print('us: ', env.us)
+            print('rewards: ', e_rewards)
+
+    #print(env.FIMs)
+            print(env.detFIMs)
+    #print(env.all_param_guesses)
+    #print(env.actual_params)
+    for i in range(5):
+        print(i)
+        print('system:', env.true_trajectory[:env.n_system_variables, i])
+        print('sensitivities:', reshape(env.true_trajectory[env.n_system_variables:env.n_system_variables+env.n_sensitivities, i], (2, 6)))
+        print('FIM:', env.get_FIM(env.true_trajectory[:, i])) #env.true_trajectory[env.n_system_variables+env.n_sensitivities:env.n_system_variables+env.n_sensitivities +env.n_FIM_elements, i])
+    np.save(save_path + 'trajectories.npy', np.array(env.true_trajectory))
+
+    np.save(save_path + 'true_trajectory.npy', env.true_trajectory)
+    # np.save(save_path + 'est_trajectory.npy', env.est_trajectory)
+    np.save(save_path + 'us.npy', np.array(env.us))
+
+    np.save(save_path + 'all_returns.npy', np.array(all_returns))
+    np.save(save_path + 'actions.npy', np.array(agent.actions))
+    np.save(save_path + 'values.npy', np.array(agent.values))
+    t = np.arange(N_control_intervals) * int(control_interval_time)
+
+    plt.plot(env.true_trajectory[0, :].elements(), label = 'true')
+    #plt.plot(env.est_trajectory[0, :].elements(), label = 'est')
+    plt.legend()
+    plt.ylabel('rna')
+    plt.xlabel('time (mins)')
+    plt.savefig(save_path + 'rna_trajectories.pdf')
+
+
+    plt.figure()
+    plt.plot( env.true_trajectory[1, :].elements(), label = 'true')
+    #plt.plot(env.est_trajectory[1, :].elements(), label = 'est')
+    plt.legend()
+    plt.ylabel( 'protein')
+    plt.xlabel('time (mins)')
+    plt.savefig(save_path + 'prot_trajectories.pdf')
+
+    '''
+    plt.figure()
+    plt.step(np.arange(len(env.us)), np.array(env.us))
+    plt.ylabel('u')
+    plt.xlabel('time (mins)')
+    '''
+
+    plt.ylim(bottom=0)
+    plt.ylabel('u')
+    plt.xlabel('Timestep')
+    plt.savefig(save_path + 'log_us.pdf')
+
+    plt.figure()
+    plt.plot(all_returns)
+    plt.ylabel('Return')
+    plt.xlabel('Episode')
+    plt.savefig(save_path + 'return.pdf')
+
+
+
+    plt.show()
--- imports/OED_env.py
+++ imports/OED_env.py
@@ -13,7 +13,7 @@ def enablePrint():
 
 class OED_env():
 
-    def __init__(self, x0, xdot, param_guesses, actual_params, num_inputs, input_bounds, dt, control_interval_time):
+    def __init__(self, x0, xdot, param_guesses, actual_params, n_observed_variables, n_controlled_inputs, num_inputs, input_bounds, dt, control_interval_time, normaliser):
 
 
         # build the reinforcement learning state
@@ -25,22 +25,24 @@ class OED_env():
         self.n_sensitivities = []
 
         self.dt = dt
-
+        self.n_observed_variables = n_observed_variables
         self.initial_params = param_guesses
         self.param_guesses = param_guesses
         self.n_params = len(self.param_guesses.elements())
-        self.n_sensitivities = self.n_system_variables * self.n_params
+        self.n_sensitivities = self.n_observed_variables * self.n_params
         self.n_FIM_elements = sum(range(self.n_params+1))
-        self.n_tot = self.n_system_variables + self.n_params*self.n_system_variables + self.n_FIM_elements
+        self.n_tot = self.n_system_variables + self.n_sensitivities + self.n_FIM_elements
         print(self.n_params, self.n_sensitivities, self.n_FIM_elements)
         print('n fim: ', self.n_FIM_elements)
+        print('n_tot: ', self.n_tot)
+        print('n_sense: ', self.n_sensitivities)
         self.x0 = x0
-
+        self.n_controlled_inputs = n_controlled_inputs
+        self.normaliser = normaliser
 
         self.initial_Y = DM([0] * (self.n_tot))
 
-        self.initial_Y[0] = 0.000001
-        self.initial_Y[1] = 0.000001  # to prevent nan
+        self.initial_Y[0:len(x0)] = x0
 
 
         self.Y = self.initial_Y
@@ -50,12 +52,13 @@ class OED_env():
 
         self.all_param_guesses = []
         self.all_RL_states = []
-        self.us = np.array([])
+        self.us = []
 
 
         self.actual_params = actual_params
+
         self.num_inputs = num_inputs
-        self.input_bounds = input_bounds
+        self.input_bounds = np.array(input_bounds)
 
         self.CI_solver  = self.get_control_interval_solver(control_interval_time, dt) # set this up here as it take ages
 
@@ -64,7 +67,8 @@ class OED_env():
         self.Y = self.initial_Y
         self.FIMs = []
         self.detFIMs = []
-        self.us = np.array([])
+        self.logdetFIMs =[]
+        self.us = []
         self.true_trajectory = []
         self.est_trajectory = []
 
@@ -73,30 +77,38 @@ class OED_env():
 
         # xdot = (sym_theta[0] * sym_u/(sym_theta[1] + sym_u))*sym_Y[0]
 
-        dx = self.xdot(Y, theta, u)
+        dx = self.xdot(Y, theta,u)
 
-        sensitivities_dot = jacobian(dx, theta) + mtimes(jacobian(dx, Y[0:2]), jacobian(Y[0:2], theta))
-
+        sensitivities_dot = jacobian(dx[0:self.n_observed_variables], theta) + mtimes(jacobian(dx[0:self.n_observed_variables], Y[0:self.n_observed_variables]), jacobian(Y[0:self.n_observed_variables], theta))
+        print(sensitivities_dot.shape)
         for i in range(sensitivities_dot.size()[0]):  # logarithmic sensitivities
             sensitivities_dot[i, :] *= theta.T
 
-        std_rna = 0.05 * Y[0]  # to stop divde by zero when conc = 0
-        std_prot = 0.05 * Y[1]
+        std = 0.05 * Y[0:self.n_observed_variables]  # to stop divde by zero when conc = 0
 
-        inv_sigma = SX.sym('sig', 2, 2)  # sigma matrix in Nates paper
-        inv_sigma[0, 1] = 0
-        inv_sigma[1, 0] = 0
-        inv_sigma[0, 0] = 1 / (std_rna * Y[0])  # *1/12.5 # inverse of diagonal matrix
-        inv_sigma[1, 1] = 1 / (std_prot * Y[1])  # *1/12.5# inverse of diagonal matrix
 
-        sensitivities = reshape(Y[self.n_system_variables:self.n_system_variables + self.n_params *self. n_system_variables],
-                                (self.n_system_variables, self.n_params))
+        inv_sigma = SX.sym('sig', self.n_observed_variables, self.n_observed_variables)  # sigma matrix in Nates paper
+
+        for i in range(self.n_observed_variables):
+            for j in range(self.n_observed_variables):
+
+                if i == j:
+                    inv_sigma[i, j] = 1/(std[i] * Y[i])
+                else:
+                    inv_sigma[i, j] = 0
+        #print(inv_sigma)
+        sensitivities = reshape(Y[self.n_system_variables:self.n_system_variables + self.n_params *self.n_observed_variables],
+                                (self.n_observed_variables, self.n_params))
         FIM_dot = mtimes(transpose(sensitivities), mtimes(inv_sigma, sensitivities))
         FIM_dot = self.get_unique_elements(FIM_dot)
 
         RHS[0:len(dx.elements())] = dx
 
         sensitivities_dot = reshape(sensitivities_dot, (sensitivities_dot.size(1) * sensitivities_dot.size(2), 1))
+
+
+        print(dx.elements())
+
         RHS[len(dx.elements()):len(dx.elements()) + len(sensitivities_dot.elements())] = sensitivities_dot
 
         RHS[len(dx.elements()) + len(sensitivities_dot.elements()):] = FIM_dot
@@ -128,7 +140,7 @@ class OED_env():
     def get_control_interval_solver(self, control_interval_time, dt):
 
         theta = SX.sym('theta', len(self.actual_params.elements()))
-        u = SX.sym('u', 1)
+        u = SX.sym('u', self.n_controlled_inputs)
 
         G_1 = self.get_one_step_RK(theta, u, dt)  # pass theta and u in just in case#
 
@@ -149,7 +161,7 @@ class OED_env():
 
     def get_full_trajectory_solver(self,  N_control_intervals, control_interval_time, dt):
         theta = SX.sym('theta', len(self.actual_params.elements()))
-        u = SX.sym('u',1)
+        u = SX.sym('u', self.n_controlled_inputs)
         G = self.get_one_step_RK(theta, u, dt)
 
         trajectory_solver = G.mapaccum('trajectory', int(N_control_intervals * control_interval_time / dt))
@@ -174,7 +186,7 @@ class OED_env():
         theta = SX.sym('theta', len(self.actual_params.elements()))
 
 
-        u = SX.sym('u', 1)
+        u = SX.sym('u', self.n_controlled_inputs)
         trajectory_solver = self.get_sampled_trajectory_solver(len(self.us) + 1)
         # self.past_trajectory_solver = self.get_trajectory_solver(self.xdot, len(self.us))
 
@@ -204,13 +216,13 @@ class OED_env():
 
 
         if test_trajectory is None:
-            trajectory = trajectory_solver(self.initial_Y, self.actual_params, self.us)
+            trajectory = trajectory_solver(self.initial_Y, self.actual_params, np.array(self.us)[:,:,0].T)
         else:
             trajectory = test_trajectory
 
-        est_trajectory_sym = trajectory_solver(self.initial_Y, sym_theta,  self.us)
+        est_trajectory_sym = trajectory_solver(self.initial_Y, sym_theta,  np.array(self.us)[:,:,0].T)
 
-        e = ((trajectory[:,0:2] - est_trajectory_sym[:,0:2])/(0.05*trajectory[:,0:2]+0.00000001)) # weighted least squares cut off initial conditions
+        e = ((trajectory[:,0:self.n_system_variables] - est_trajectory_sym[:,0:self.n_system_variables])/(0.05*trajectory[:,0:self.n_system_variables]+0.00000001)) # weighted least squares cut off initial conditions
         nlp = {'x':sym_theta, 'f':0.5*dot(e,e)}
 
         solver = self.gauss_newton(e, nlp, sym_theta)
@@ -220,31 +232,19 @@ class OED_env():
         return solver
 
     def step(self, action = None):
-        #TODO: COVARIANCE MATRIX IN FIM
-        #       KEEP SEQUENCE OF FIMS AND ADD TO IT, ALTHOUGH DONT HAVE TO DO THIS IF USING COMPUTATIONAL SAVING APPROX?
-        #       SCALE FIM LIKE IN NATE'S PAPER
+
 
         if action is None: # Traditional OED step
             u_solver = self.get_u_solver()
             u = u_solver(x0=self.u0, lbx = 10**self.input_bounds[0], ubx = 10**self.input_bounds[1])['x']
-            self.us = np.append(self.us, u)
+            self.us.append(u)
         else: #RL step
             u = self.action_to_input(action)
-            self.us = np.append(self.us, 10**u)
+            #self.us.append(10**u)
+            self.us.append(u)
 
-        '''
-        logus = [1,-3,2,-3,3,-3]
-        inputs = []
-        logus = (np.random.rand(6, ) - 0.5) * 3
-        us = 10. ** np.array(logus)
-        #us = np.random.rand(1,6) * 1000
-        for u in us:
-            inputs.extend([u] * 2)  # int(control_interval_time * 2 / dt))
-
-        print(us)
-        self.us = np.array(inputs)
-        '''
-
+        print(action)
+        print(u)
 
         N_control_intervals = len(self.us)
         #N_control_intervals = 12
@@ -256,7 +256,8 @@ class OED_env():
         #trajectory_solver = self.get_full_trajectory_solver(N_control_intervals, control_interval_time, self.dt) # the true trajectory of the system
         #trajectory_solver = trajectory_solver(N_control_intervals, control_interval_time, dt ) #this si the symbolic trajectory
         t = time.time()
-        self.true_trajectory = sampled_trajectory_solver(self.initial_Y,  self.actual_params, self.us)
+        print(np.array(self.us).shape)
+        self.true_trajectory = sampled_trajectory_solver(self.initial_Y,  self.actual_params, np.array(self.us)[:,:,0].T)
 
         #self.est_trajectory = sampled_trajectory_solver(self.initial_Y, self.param_guesses, self.us )
 
@@ -293,6 +294,7 @@ class OED_env():
         logdet_FIM = trace(log(r)).elements()[0] # do it like this to protect from numerical errors from multiplying large EVs
 
         if det_FIM <= 0:
+            print('----------------------------------------smaller than 0')
             eigs = np.real(np.linalg.eig(FIM)[0])
             eigs[eigs<0] = 0.00000000000000000000000001
             det_FIM = np.prod(eigs)
@@ -308,6 +310,7 @@ class OED_env():
             #print('det adfa: ', det_FIM)
             #print(det_FIM - self.detFIMs[-2])
         except:
+            print('HELLO')
             reward = logdet_FIM
 
         if math.isnan(reward):
@@ -337,7 +340,7 @@ class OED_env():
 
         # calculate which bucket each eaction belongs in
 
-        buckets = np.unravel_index(action, [self.num_inputs])
+        buckets = np.unravel_index(action, [self.num_inputs] *self.n_controlled_inputs)
 
         # convert each bucket to a continuous state variable
         Cin = []
@@ -351,7 +354,7 @@ class OED_env():
     def get_FIM(self, trajectory):
 
         # Tested on 2x2 and 5x5 matrices
-        FIM_start = self.n_system_variables + self.n_params * self.n_system_variables
+        FIM_start = self.n_system_variables + self.n_sensitivities
 
         FIM_end = FIM_start + self.n_FIM_elements
 
@@ -395,7 +398,7 @@ class OED_env():
         return UE
 
     def normalise_RL_state(self, state):
-        return state / np.array([1e3, 1e4, 1e2, 1e6, 1e10, 1e-3, 1e1, 1e9, 1e9, 1e9, 1e9, 1, 1e9, 1e9, 1e9, 1, 1e9, 1e9, 1, 1e9, 1, 1e7,10, 100])
+        return state / self.normaliser
 
     def get_RL_state(self, true_trajectory, est_trajectory):
 
@@ -404,7 +407,7 @@ class OED_env():
         sys_state = true_trajectory[:self.n_system_variables, -1] #TODO: measurement noise
 
         # get current fim elements
-        FIM_start = self.n_system_variables + self.n_params * self.n_system_variables
+        FIM_start = self.n_system_variables + self.n_sensitivities
 
         FIM_end = FIM_start + self.n_FIM_elements
 
@@ -423,8 +426,8 @@ class OED_env():
 
     def get_initial_RL_state(self):
         state = np.array(self.x0 + self.param_guesses.elements() + [0] * self.n_FIM_elements)
-        state = np.append(state, 0)
-        state = np.append(state, 0)
+        state = np.append(state, 0) #time
+        state = np.append(state, 0) #logdetFIM
 
         return self.normalise_RL_state(state)
 
