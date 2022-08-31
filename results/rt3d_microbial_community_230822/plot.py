71332d527598228612e71576aa16edda9b12d388
Neythen <zcqsntr@ucl.ac.uk>
2022-08-31 14:22:53 +0100

    test for RT3d GT

---
+++ results/rt3d_microbial_community_230822/plot.py
@@ -0,0 +1,183 @@
+import matplotlib as mpl
+mpl.use('tkagg')
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import numpy as np
+
+SMALL_SIZE = 11
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 17
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
+def get_rate( episode, MIN_RATE, MAX_RATE, denominator):
+    '''
+    Calculates the logarithmically decreasing explore or learning rate
+
+    Parameters:
+        episode: the current episode
+        MIN_LEARNING_RATE: the minimum possible step size
+        MAX_LEARNING_RATE: maximum step size
+        denominator: controls the rate of decay of the step size
+    Returns:----------------------------------------------------------------------------------------------------------+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        step_size: the Q-learning step size
+    '''
+
+    # input validation
+    if not 0 <= MIN_RATE <= 1:
+        raise ValueError("MIN_LEARNING_RATE needs to be bewteen 0 and 1")
+
+    if not 0 <= MAX_RATE <= 1:
+        raise ValueError("MAX_LEARNING_RATE needs to be bewteen 0 and 1")
+
+    if not 0 < denominator:
+        raise ValueError("denominator needs to be above 0")
+
+    rate = max(MIN_RATE, min(MAX_RATE, 1.0 -math.log10((episode + 1) / denominator)))
+
+    return rate
+
+
+
+path = '.'
+
+step = 500
+n_repeats = 3
+
+
+
+
+all_returns = []
+all_us = []
+all_trajectories =[]
+
+for i in [1,3,4,5,6,7,8,10]:
+    '''
+    us = np.load(path + '/repeat' + str(i) +'/us.npy')
+    print(us.shape)
+    all_us.append(us)
+    t = np.arange(0, N_control_intervals + 1) * control_interval_time  # int(control_interval_time / dt)) * dt
+    plt.figure()
+    print(us[0,:, :].T)
+    print(us[:, :, 0].T)
+    #us = np.vstack((us[0,:, :].T,us[:, :, 0]))
+
+    print(us.shape)
+    #plt.step(t, us[:,0], ':', color='red', label='$C_{in}$')
+    #plt.step(t, us[:,1], ':', color='black', label='$C_{0, in}$')
+    #plt.legend()
+
+    #plt.ylabel('u')
+    #plt.xlabel('Time (min)')
+
+    
+    trajectory = np.load(path + '/repeat' + str(i) +'/trajectory.npy')
+    all_trajectories.append(trajectory)
+
+    print(trajectory.shape)
+    t = np.arange(1, N_control_intervals + 1) * (100)  # int(control_interval_time / dt)) * dt
+    fig, ax1 = plt.subplots()
+    ax1.plot(t, trajectory[0, :], label='Population')
+    ax1.set_ylabel('Population ($10^5$ cells/L)')
+    ax1.set_xlabel('Time (min)')
+
+    ax2 = ax1.twinx()
+    ax2.plot(t, trajectory[1, :], color='red', label='C')
+    ax2.set_ylabel('C ($g/L$)')
+    ax2.set_xlabel('Time (min)')
+
+    ax2.plot(t, trajectory[2, :], color='black', label='$C_0$')
+    ax2.set_ylabel('Concentration ($g/L$)')
+    ax2.set_xlabel('Time (min)')
+    fig.tight_layout()
+    fig.legend(bbox_to_anchor=(0.8, 0.9))
+
+    '''
+    print()
+    print(i)
+
+    returns = np.load(path + '/repeat' + str(i) +'/all_returns.npy')*100
+    print('end:', returns[-1])
+    print('max:', np.max(returns))
+
+    actions = np.load(path + '/repeat' + str(i) +'/actions.npy')
+    #print('actions', actions[-10:])
+    #print('actions', actions.shape)
+
+    y = [np.mean(returns[i * step: (i + 1) * step]) for i in range(0, len(returns) // step)]
+    print(i, 'everage max ', y[-1])
+    #y.append(returns[-1])
+    #plt.figure()
+    #plt.plot(y)
+
+    # i in [1,2,3]:
+    all_returns.append(y)
+
+    #values =  np.load(path + '/repeat' + str(i) +'/values.npy')
+    n_unstable =  np.load(path + '/repeat' + str(i) +'/n_unstables.npy')
+    print('unstable:', n_unstable[-1])
+    #plt.plot(n_unstable)
+    #plt.show()
+
+
+#print(values[-1,0,:])
+
+
+#plt.close('all')
+
+
+all_returns = np.array(all_returns)
+all_us = np.array(all_us)
+all_trajectories = np.array(all_trajectories)
+print(all_returns.shape, all_us.shape, all_trajectories.shape)
+#print(all_returns[:, -1])
+x = [(i+1) * step for i in range(0, len(returns)//step)]
+#x.append(len(returns)+step)
+print(len(x), len(y))
+
+
+episodes = np.arange(1, len(returns) + 1)   # int(control_interval_time / dt)) * dt
+explore_rates = [get_rate(episode, 0, 1, len(returns)/11) for episode in episodes]
+print(explore_rates[-100:])
+
+fig, ax1 = plt.subplots()
+for i in range(len(all_returns)):
+    print(len(all_returns[i]))
+ax1.errorbar(np.array(x), np.mean(np.array(all_returns), axis = 0), np.std(np.array(all_returns), axis = 0), label = 'Average Return')
+#plt.plot(x,all_returns[0])
+#plt.plot(x,all_returns[1])
+#plt.plot(x,all_returns[2])
+
+#plt.plot(len(returns)+step,  16.612377905628856, 'o', label = 'OSAO = 16.61')
+##plt.plot(len(returns)+step, 15.2825, 'o', label = 'Rational = 15.28')
+#plt.plot(len(returns)+step, 20.27, 'o', label = 'Best RL = 20.27', color='C0')
+#plt.plot(len(returns)+step, 20.07, 'o', label = 'MPC = 20.07')
+ax1.set_ylabel('Return')
+ax1.set_xlabel('Episode')
+#ax1.set_ylim(bottom=26)
+
+
+ax2 = ax1.twinx()
+ax2.plot(episodes, explore_rates, color = 'black', label = 'Explore rate')
+
+
+ax2.set_ylabel('Explore Rate')
+ax2.set_xlabel('Episode')
+plt.tight_layout()
+
+ax1.legend(loc=(0.2, 0.9))
+ax2.legend(loc=(0.2,0.83))
+#plt.title('LR: ' + str(pol_learning_rate) + ' ' + 'Layer sizes: ' + str(hidden_layer_size))
+plt.savefig('./plot.pdf')
+plt.show()
+
+
+
